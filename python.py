ea49ad75338f4a9703375e5e44804d74a7c4d75b
tdlsaruar <saruarahmed321@gmail.com>
2025-09-16 18:51:01 +0300

    Initial commit: add foundation.py

---
+++ python.py
@@ -0,0 +1,17 @@
+print ("Hello, python this is my first code")
+
+print ("This is my name")
+# This is my note amar jodi python run korthe hoy taile print ("") other wise code kaj korbena 
+
+
+
+name = input("Enter your name: ")
+print ("Hello how are you " + name)
+
+
+
+age =18
+if age >= 18:
+    print ("You are eligible for vote")
+else :
+    print("You are not eligible for vote")
